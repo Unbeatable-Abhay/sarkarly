2c518706e555ce5a88325684cdebe85445466ee9
Abhay <268219626+Unbeatable-Abhay@users.noreply.github.com>
2026-06-08 01:45:25 +0530

    Deleted the initial testing file (test.py)

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.post(
-    "http://localhost:5000/home",
-    json={"query": "I am a farmer with 5 acres in UP, which schemes can I get?"}
-)
-
-print(response.json())
